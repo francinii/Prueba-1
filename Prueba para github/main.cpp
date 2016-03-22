caf02da14d70466f414adf4915fec7d093674174
TsukishimaKaori <nubeblanca1997@gmail.com>
2016-03-22 14:05:23 -0600

    Una prueba
    Porque si

---
+++ Prueba para github/main.cpp
@@ -0,0 +1,11 @@
+
+#include <cstdlib>
+#include <iostream>
+        
+
+int main(int argc, char** argv) {
+
+    std::cout<<"Esta es una prueba muy bonita para ver si compila esta cosa"<<std::endl;
+    return 0;
+}
+
