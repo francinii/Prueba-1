47b77d371caed3264b4aea6bbb5e6ca561f45832
TsukishimaKaori <nubeblanca1997@gmail.com>
2016-03-22 14:28:34 -0600

    he agregado
    esto

--- Prueba para github/main.cpp
+++ Prueba para github/main.cpp
@@ -6,6 +6,7 @@
 int main(int argc, char** argv) {
 
     std::cout<<"Esta es una prueba muy bonita para ver si compila esta cosa"<<std::endl;
+    std::cout<<"Le hice unos cambios raritos"<<std::endl;
     return 0;
 }
 
